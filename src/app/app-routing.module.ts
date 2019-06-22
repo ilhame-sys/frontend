b4cf1f944222d15b6c8b009b759cdfa01ff41abc
Dyweam <51490460+Dyweam@users.noreply.github.com>
2019-06-22 12:24:08 +0200

    MàJ header
    - Ajout des routerLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { OrganizationComponent } from './component/organization/organization.component';
+import { CauseComponent } from './component/cause/cause.component';
+import { ArticleComponent } from './component/article/article.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { MembershipComponent } from './component/membership/membership.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: 'organization',
+    component: OrganizationComponent,
+  },
+  {
+    path: 'cause',
+    component: CauseComponent,
+  },
+  {
+    path: 'article',
+    component: ArticleComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'membership',
+    component: MembershipComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
